edd12393863361c4e4f40b2a458af89aaa076f7a
TrailerDerdan <samsonov.semyon97@gmail.com>
2023-10-08 21:50:18 +0600

    Корректировка

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -10,60 +10,55 @@ class PrintEditionItem {
   fix() {
     this.state *= 1.5;
   }
+
   set state(newState) {
     if (newState < 0) {
       this._state = 0;
     } else if (newState > 100) {
       this._state = 100;
-    } else this._state = newState;
+    } else {
+      this._state = newState;
+    }
   }
+
   get state() {
     return this._state;
   }
 }
-const sherlock = new PrintEditionItem(
-  'Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе',
-  2019,
-  1008
-);
-
-console.log(sherlock.releaseDate);
-console.log(sherlock.state);
-sherlock.fix();
-console.log(sherlock.state);
 
 class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
+  constructor(author, name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount);
     this.type = 'magazine';
+    this.author = author;
   }
 }
 
 class Book extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount) {
+  constructor(author, name, releaseDate, pagesCount) {
     super(name, releaseDate, pagesCount);
     this.type = 'book';
-    this.name = 'author';
+    this.author = author;
   }
 }
 
 class NovelBook extends Book {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = 'novel';
   }
 }
 
 class FantasticBook extends Book {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = 'fantastic';
   }
 }
 
 class DetectiveBook extends Book {
-  constructor(name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = 'detective';
   }
 }
@@ -82,7 +77,7 @@ picknick.fix();
 console.log(picknick.state);
 
 class Library {
-  constructor(name, books) {
+  constructor(name) {
     this.name = name;
     this.books = [];
   }
@@ -92,50 +87,29 @@ class Library {
       this.books.push(book);
     }
   }
+
   findBookBy(type, value) {
     for (let book of this.books) {
-      if (book[type] === value) {
+      if (type === 'type' && book.type === value) {
         return book;
-      } else {
-        return null;
+      } else if (type === 'author' && book.author === value) {
+        return book;
+      } else if (type === 'name' && book.name === value) {
+        return book;
+      } else if (type === 'releaseDate' && book.releaseDate === value) {
+        return book;
       }
     }
+    return null;
   }
+
   giveBookByName(bookName) {
-    for (let i = 0; i < this.books.length; i++) {
-      if (this.books[i].name === bookName) {
-        return this.books.splice(i, 1).pop();
-      } else {
-        return null;
+    for (let book of this.books) {
+      if (book.name === bookName) {
+        this.books.splice(this.books.indexOf(book), 1);
+        return book;
       }
     }
+    return null;
   }
 }
-
-const library = new Library('Библиотека имени Ленина');
-
-library.addBook(
-  new DetectiveBook(
-    'Артур Конан Дойл',
-    'Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе',
-    2019,
-    1008
-  )
-);
-library.addBook(
-  new FantasticBook(
-    'Аркадий и Борис Стругацкие',
-    'Пикник на обочине',
-    1972,
-    168
-  )
-);
-library.addBook(new NovelBook('Герберт Уэллс', 'Машина времени', 1895, 138));
-library.addBook(new Magazine('Мурзилка', 1924, 60));
-
-console.log(library.findBookBy('name', 'Властелин колец')); //null
-console.log(library.findBookBy('releaseDate', 1924).name); //"Мурзилка"
-
-console.log('Количество книг до выдачи: ' + library.books.length); //Количество книг до выдачи: 4
-library.giveBookByName('Машина времени');
-console.log('Количество книг после выдачи: ' + library.books.length); //Количество книг после выдачи: 3
