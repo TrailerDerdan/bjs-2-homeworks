9f1eda631f51e800964d9c1a6c0f549023dc38d2
TrailerDerdan <113605515+TrailerDerdan@users.noreply.github.com>
2024-07-25 09:50:09 +0600

    исправлены 1 и 3 задачи

--- tooltip/task.js
+++ tooltip/task.js
@@ -1,16 +1,32 @@
 const hasTooltip = document.querySelectorAll('.has-tooltip');
-hasTooltip.forEach((e) => {
-  e.addEventListener('click', () => {
-    let tooltip = e.querySelector('.tooltip');
+let tooltip; 
 
-    if (tooltip) {
-      tooltip.classList.toggle('tooltip_active');
-    } else {
-      let tooltipText = e.getAttribute('title');
-      e.insertAdjacentHTML(
-        'beforeend',
-        `<div class="tooltip tooltip_active">${tooltipText}</div>`
-      );
+hasTooltip.forEach((el) => {
+  el.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    if (!tooltip) { 
+      tooltip = document.createElement('div');
+      tooltip.className = 'tooltip';
+      document.body.appendChild(tooltip);
     }
+
+    let tooltipText = el.getAttribute('title');
+    tooltip.innerText = tooltipText;
+
+    let rect = el.getBoundingClientRect();
+    tooltip.style.position = 'absolute';
+    tooltip.style.left = `${rect.left}px`;
+    tooltip.style.top = `${rect.bottom + 5}px`;
+
+    tooltip.classList.toggle('tooltip_active'); 
   });
 });
+
+
+document.addEventListener('click', function(event) {
+  if (tooltip && !event.target.classList.contains('has-tooltip')) {
+    tooltip.classList.remove('tooltip_active');
+  }
+});
+
