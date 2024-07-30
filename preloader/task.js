5741fcce6467dd2fc452e2ff90b62f170239842d
TrailerDerdan <113605515+TrailerDerdan@users.noreply.github.com>
2024-07-30 10:24:21 +0600

    Add files via upload

--- preloader/task.js
+++ preloader/task.js
@@ -1,17 +1,27 @@
 let preloader = document.getElementById('loader');
+const itemsContainer = document.getElementById('items');
 
 const xhr = new XMLHttpRequest();
 
 xhr.addEventListener('readystatechange', () => {
-if (xhr.readyState === xhr.DONE) {
-let answer = JSON.parse(xhr.responseText, function(key, value) {
-if (key === 'Valute') return new Array(value);
-return value;
+    if (xhr.readyState === xhr.DONE) {
+        let answer = JSON.parse(xhr.responseText);
+        preloader.classList.remove('loader_active');
+        let valutes = answer.response.Valute;
+        for (let key in valutes) {
+            if (valutes.hasOwnProperty(key)) {
+                let valute = valutes[key];
+                let itemDiv = document.createElement('div');
+                itemDiv.className = 'item';
+                itemDiv.innerHTML = `
+                    <div class="item__code">${valute.CharCode}</div>
+                    <div class="item__value">${valute.Value}</div>
+                    <div class="item__currency">руб.</div>
+                `;
+                itemsContainer.appendChild(itemDiv);
+            }
+        }
+    }
 });
-preloader.classList.remove('loader_active');
-}
-});
-
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
 xhr.send();
-
